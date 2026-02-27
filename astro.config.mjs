242648d4378c3d884c2b72aecb6490bdb42f19a9
Kabir <kabiralberto10@hotmail.com>
2026-02-27 15:16:06 -0500

    fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-    adapter: node({ mode: 'standalone' }),
     integrations: [react(), markdoc()],
     vite: {
         plugins: [tailwindcss()],
