2dcdbf693c18b99a7257d2be27cb333e343a9e5d
Kabir <kabiralberto10@hotmail.com>
2026-02-27 14:54:46 -0500

    remove keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
-import keystatic from '@keystatic/astro';
 import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
     adapter: node({ mode: 'standalone' }),
-    integrations: [react(), markdoc(), keystatic()],
+    integrations: [react(), markdoc()],
     vite: {
         plugins: [tailwindcss()],
     },
